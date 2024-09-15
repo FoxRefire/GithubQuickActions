282f1c024a045c3d10be6dbc965b2f75dbf33111
FoxRefire <155989196+FoxRefire@users.noreply.github.com>
2024-09-15 17:19:59 +0900

    Initial commit

---
+++ content.js
@@ -0,0 +1,35 @@
+function waitTargetElement(){
+    return new Promise(resolve => {
+        let interval  = setInterval(() => {
+            if(document.querySelector(".AppHeader-actions")){
+                resolve()
+                clearInterval(interval)
+            }
+        }, 100)
+    })
+}
+
+function getUserName(){
+    return document.querySelector("meta[name='user-login']").content
+}
+
+function writeQuickActionElement(name, href, icon, userName){
+    let hrefReplaced = href.replace("$NAME", userName)
+    document.querySelector(".AppHeader-actions").insertAdjacentHTML("beforeend",`
+        <a title="${name}" href="${hrefReplaced}" data-view-component="true" class="Button Button--iconOnly Button--secondary Button--medium AppHeader-button color-fg-muted">
+            ${icon}
+        </a>
+    `)
+}
+async function main(){
+    await waitTargetElement()
+    let actions = await fetch(chrome.runtime.getURL("./actions.json")).then(e => e.json())
+    let currentItems = await chrome.storage.local.get("currentItems").then(e => e?.currentItems) || ["issues", "pulls"]
+    let userName = getUserName()
+
+    currentItems.forEach(item => {
+        let itemDict = actions.find(e => item == e.id)
+        writeQuickActionElement(itemDict.name, itemDict.href, itemDict.icon, userName)
+    })
+}
+main()
---
+++ popup.js
@@ -0,0 +1,59 @@
+let currentItems = await chrome.storage.local.get("currentItems").then(e => e?.currentItems) || ["issues", "pulls"]
+
+let actions = await fetch("./actions.json").then(e => e.json())
+actions.forEach(action => {
+    document.getElementById("items").insertAdjacentHTML("beforeend",`
+        <input id="${action.id}" type="checkbox"/>
+        <label for="${action.id}">${action.name}</label><br>
+    `)
+    document.getElementById(action.id).checked =  currentItems.includes(action.id)
+    document.getElementById(action.id).addEventListener("change", e => {
+        e.target.checked ? addItem(e.target.id) : removeItem(e.target.id)
+    })
+})
+
+writeTable()
+
+function addItem(id){
+    currentItems.push(id)
+    writeTable()
+}
+
+function removeItem(id){
+    currentItems = currentItems.filter(item => item !== id)
+    writeTable()
+}
+
+function upItem(index){
+    if(currentItems[index - 1]){
+        let move = currentItems.splice(index, 1)
+        currentItems.splice(index - 1, 0, move)
+        writeTable()
+    }
+}
+
+function downItem(index){
+    if(currentItems[index + 1]){
+        let move = currentItems.splice(index, 1)
+        currentItems.splice(index + 1, 0, move)
+        writeTable()
+    }
+}
+
+function writeTable(){
+    let target = document.getElementById("currentItems")
+    target.innerHTML = ""
+    chrome.storage.local.set({"currentItems": currentItems})
+    currentItems.forEach((item, index) => {
+        let itemDict = actions.find(e => item == e.id)
+        target.insertAdjacentHTML("beforeend",`
+            <tr>
+                <th>${itemDict.name}</th>
+                <th><a id="up${item}" href="#!">↑</a></th>
+                <th><a id="down${item}"  href="#!">↓</a></th>
+            </tr>
+        `)
+        document.getElementById(`up${item}`).addEventListener("click", () => upItem(index))
+        document.getElementById(`down${item}`).addEventListener("click", () => downItem(index))
+    })
+}
