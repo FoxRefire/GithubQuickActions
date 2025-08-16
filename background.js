ee1043dea9a5dc5d91a8b397ceb371d36917d6d5
FoxRefire <155989196+FoxRefire@users.noreply.github.com>
2025-08-16 23:35:05 +0900

    Open dashboard when install

--- background.js
+++ background.js
@@ -1,3 +1,9 @@
 chrome.action.onClicked.addListener(() => {
     chrome.tabs.create({url: "popup.html"})
 })
+
+chrome.runtime.onInstalled.addListener((details) => {
+    if (details.reason === "install") {
+        chrome.tabs.create({url: "popup.html"})
+    }
+})
